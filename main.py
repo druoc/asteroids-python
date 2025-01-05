2f2112723e45b2994173094f541e87a7a08a6aed
Dave <d.ruocco1987@googlemail.com>
2025-01-05 00:56:46 +0000

    Adds rotate and update functions to Player class

--- main.py
+++ main.py
@@ -17,10 +17,12 @@ def main():
                 return
             
         screen.fill((0,0,0))
+        dt = fps_clock.tick(60) / 1000
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
 
-        dt = fps_clock.tick(60) / 1000
+        
     
 if __name__ == "__main__":
     main()
