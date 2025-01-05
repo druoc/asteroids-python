4e1690b0baf2017b9b2c613cb71d55abd7c12113
Dave <d.ruocco1987@googlemail.com>
2025-01-05 00:21:55 +0000

    Adds Player class and rendering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 fps_clock = pygame.time.Clock()
 dt = 0
@@ -7,16 +8,19 @@ dt = 0
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit()
                 return
             
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
-        fps_clock.tick(60)
+
         dt = fps_clock.tick(60) / 1000
-    
     
 if __name__ == "__main__":
     main()
