517425dde0815355d0af03f67f4ad20c1f606f3c
druoc <d.ruocco1987@googlemail.com>
2025-01-10 15:22:59 +0000

    Adds drawable and updatabale groups

--- main.py
+++ main.py
@@ -8,21 +8,29 @@ dt = 0
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    
+    dt = 0
+        
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
             
+        for obj in updatable:
+            obj.update(dt)
+            
         screen.fill((0,0,0))
-        dt = fps_clock.tick(60) / 1000
-        player.update(dt)
-        player.draw(screen)
-        pygame.display.flip()
-
         
+        for obj in drawable:
+            obj.draw(screen)
+            
+        dt = fps_clock.tick(60) / 1000
+        pygame.display.flip()
     
 if __name__ == "__main__":
     main()
