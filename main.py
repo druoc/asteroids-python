cdec586c2bb93bf6d59b05292d228f0d5143736c
druoc <d.ruocco1987@googlemail.com>
2025-01-13 14:05:01 +0000

    Adds asteroid and asteroidfield classes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 fps_clock = pygame.time.Clock()
 dt = 0
@@ -11,7 +13,14 @@ def main():
         
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
+    AsteroidField.containers = updatable
+    
+    asteroid_field = AsteroidField()
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
         
